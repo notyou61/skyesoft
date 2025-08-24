0e555591f2b4e71bc38b8762ce4e835bebe989e2
Steve Skye <steve.skye@skyelighting.com>
2025-08-24 08:58:06 -0700

    Refactor: Unified quick action handler with performLogout() and AI JSON support

--- api/askOpenAI.php
+++ api/askOpenAI.php
@@ -491,7 +491,7 @@ function performLogout() {
     if (ini_get("session.use_cookies")) {
         $params = session_get_cookie_params();
         setcookie(session_name(), '', time() - 42000,
-            $params["path"], $params["domain"], $params["secure"], $params["httponly"] ?? false
+            $params["path"], $params["domain"], $params["secure"], isset($params["httponly"]) ? $params["httponly"] : false
         );
     }
     setcookie('skyelogin_user', '', time() - 3600, '/', 'www.skyelighting.com');
