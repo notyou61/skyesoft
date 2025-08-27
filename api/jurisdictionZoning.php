52d87b2e2688ac69ec3c1e61e4524b0c73ce3686
Steve Skye <steve.skye@skyelighting.com>
2025-08-27 09:40:33 -0700

    Fix Phoenix zoning lookup (use ZONING + GEN_ZONE)

--- api/jurisdictionZoning.php
+++ api/jurisdictionZoning.php
@@ -6,19 +6,30 @@ function getJurisdictionZoning($jurisdiction, $latitude, $longitude) {
     $zoning = "CONTACT LOCAL JURISDICTION"; // default
 
     switch (strtoupper($jurisdiction)) {
+        // Phoenix Metro Area Examples
         case "PHOENIX":
-            $url = "https://services.arcgis.com/DO4gTjwJVIJ7O9Ca/arcgis/rest/services/Zoning/FeatureServer/0/query"
-                 . "?f=json&geometry=" . $longitude . "," . $latitude
-                 . "&geometryType=esriGeometryPoint&inSR=4326&spatialRel=esriSpatialRelIntersects&outFields=ZONE";
+            $url = "https://maps.phoenix.gov/pub/rest/services/Public/Zoning/MapServer/0/query"
+                . "?f=json"
+                . "&geometry=" . urlencode('{"x":' . $longitude . ',"y":' . $latitude . '}')
+                . "&geometryType=esriGeometryPoint"
+                . "&inSR=4326"
+                . "&spatialRel=esriSpatialRelIntersects"
+                . "&outFields=ZONING,GEN_ZONE,LABEL1"
+                . "&returnGeometry=false";
+
             $resp = @file_get_contents($url);
             if ($resp !== false) {
                 $data = json_decode($resp, true);
-                if (!empty($data['features'][0]['attributes']['ZONE'])) {
-                    $zoning = $data['features'][0]['attributes']['ZONE'];
+                if (!empty($data['features'][0]['attributes']['ZONING'])) {
+                    $attrs = $data['features'][0]['attributes'];
+                    $zoning = $attrs['ZONING'];
+                    if (!empty($attrs['GEN_ZONE'])) {
+                        $zoning .= " (" . $attrs['GEN_ZONE'] . ")";
+                    }
                 }
             }
             break;
-
+        // Mesa Example
         case "MESA":
             $url = "https://services2.arcgis.com/Uq9r85Potqm3MfRV/arcgis/rest/services/Zoning/FeatureServer/0/query"
                  . "?f=json&geometry=" . $longitude . "," . $latitude
@@ -31,7 +42,7 @@ function getJurisdictionZoning($jurisdiction, $latitude, $longitude) {
                 }
             }
             break;
-
+        // Gilbert Example
         case "GILBERT":
             $url = "https://gis.gilbertaz.gov/arcgis/rest/services/Zoning/MapServer/0/query"
                  . "?f=json&geometry=" . $longitude . "," . $latitude
