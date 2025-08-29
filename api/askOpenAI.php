113d8f800f5caf54dfd71df7ca3bb9501fd1af58
Steve Skye <steve.skye@skyelighting.com>
2025-08-29 08:06:19 -0700

    Integrate Gilbert zoning (MapServer/8), update askOpenAI.php for jurisdiction handling, and add Gilbert disclaimers

--- api/askOpenAI.php
+++ api/askOpenAI.php
@@ -950,6 +950,8 @@ function handleReportRequest($prompt, $reportTypes, &$conversation) {
             $context['phoenix'] = true;
         } elseif ($j === "MESA") {
             $context['mesa'] = true;
+        } elseif ($j === "GILBERT") {
+            $context['gilbert'] = true;
         } else {
             $context['unsupportedJurisdiction'] = true;
         }
