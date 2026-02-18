110b0381dda8b0f9465172007e676173d6fa8bfe
Steve Skye <steve.skye@skyelighting.com>
2026-02-18 07:48:13 -0700

    Update: update connectionTest.php

--- connectionTest.php
+++ connectionTest.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost", "notyou64_skyesoft_user", "TestPass123!", "skyesoft");
+$mysqli = new mysqli("localhost", "notyou64", "TestPass123!", "skyesoft");
 
 if ($mysqli->connect_error) {
     die("MySQLi failed: " . $mysqli->connect_error);
