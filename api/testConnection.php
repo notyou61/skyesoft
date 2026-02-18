a93da68ab9fe7c900ab0d78bb38af0b9507d2e4d
Steve Skye <steve.skye@skyelighting.com>
2026-02-18 08:29:35 -0700

    Update: update api/testConnection.php

--- api/testConnection.php
+++ api/testConnection.php
@@ -1,8 +1,4 @@
 <?php
 require 'dbConnect.php';
 
-$result = $databaseConnection->query("SELECT DATABASE() AS current_db");
-
-$row = $result->fetch_assoc();
-
-echo "Active database: " . $row['current_db'];
+echo "Connected to skyesoft successfully.";
