7e1143829354d43a7963a605509c10b9aceb0522
Steve Skye <steve.skye@skyelighting.com>
2026-03-04 10:34:30 -0700

    Update: update tools/hash.php

--- tools/hash.php
+++ tools/hash.php
@@ -1,21 +1,11 @@
 <?php
-// The plain-text password submitted by the user
-$submittedPassword = 'TempPass!2026';
+declare(strict_types=1);
 
-// The hash retrieved from your database
-$storedHash = '$2y$10$3Xv0/1EU7De/6se.Jv9Zau4kWGeEv6M9gpNJ6xUJWHHzDxPXHkSpi';
+header("Content-Type: text/plain; charset=UTF-8");
 
-// Verify the password
-if (password_verify($submittedPassword, $storedHash)) {
-    echo 'Password is valid!';
+// Change this ONE time, copy result, then delete this file.
+$plaintextPassword = 'TempPass!2026';
 
-    // Optional: Check if the password needs rehashing (e.g., if the cost factor was updated)
-    if (password_needs_rehash($storedHash, PASSWORD_DEFAULT)) {
-        // Create a new hash and update the user record in the database
-        $newHash = password_hash($submittedPassword, PASSWORD_DEFAULT);
-        // ... (code to update database) ...
-    }
+$options = ['cost' => 12];
 
-} else {
-    echo 'Invalid password.';
-}
+echo password_hash($plaintextPassword, PASSWORD_BCRYPT, $options);
