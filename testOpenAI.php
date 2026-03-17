1e873f5a5d25fca812eedcdefadaa3c9f995ad59
Steve Skye <steve.skye@skyelighting.com>
2026-03-17 09:36:10 -0700

    Update: update testOpenAI.php

--- testOpenAI.php
+++ testOpenAI.php
@@ -7,6 +7,9 @@ skyesoftLoadEnv();
 
 $apiKey = skyesoftGetEnv("OPENAI_API_KEY");
 
+error_log('[env] DB_USER=' . var_export(skyesoftGetEnv("DB_USER"), true));
+error_log('[env] DB_PASS=' . var_export(skyesoftGetEnv("DB_PASS"), true));
+
 $response = callOpenAI(
     "Say 'Skyesoft test successful.'",
     $apiKey
