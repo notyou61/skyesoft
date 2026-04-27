180bd0e192a329668dd3acf67770341f47a25f20
Steve Skye <notyou61@gmail.com>
2026-04-26 19:54:13 -0700

    Update: update api/test_dns.php

--- api/test_dns.php
+++ api/test_dns.php
@@ -1,7 +1,7 @@
 <?php
 
-echo "DNS RESULT: " . gethostbyname("api.kalshi.com");
-
 curl_setopt($ch, CURLOPT_RESOLVE, [
     "api.kalshi.com:443:34.120.XXX.XXX"
 ]);
+
+echo "DNS RESULT: " . gethostbyname("api.kalshi.com");
