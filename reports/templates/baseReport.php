3022c6dde82571e02528585b0eaa10319bdcd783
Steve Skye <steve.skye@skyelighting.com>
2026-07-07 08:47:59 -0700

    Update: update reports/templates/baseReport.php

--- reports/templates/baseReport.php
+++ reports/templates/baseReport.php
@@ -20,7 +20,7 @@ function renderReport(array $report): string
             'format'        => 'Letter',
             'margin_left'   => 12,
             'margin_right'  => 12,
-            'margin_top'    => 22,
+            'margin_top'    => 24,
             'margin_bottom' => 20,
             'margin_header' => 8,
             'margin_footer' => 8,
