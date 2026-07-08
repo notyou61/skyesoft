48376285032d7377123bea0699104394fe8f47d0
Steve Skye <notyou61@gmail.com>
2026-07-08 06:04:18 -0700

    Update: update api/testAPN.php

--- api/testAPN.php
+++ api/testAPN.php
@@ -1,17 +1,22 @@
 <?php
 declare(strict_types=1);
+
 $apn = '17342369A';
 
 $url = 'https://mcassessor.maricopa.gov/mapid/parcel/' . urlencode($apn);
 
-$response = file_get_contents($url);
+$response = @file_get_contents($url);
 
-echo '<h2>MapID Response</h2>';
-echo '<pre>';
-echo htmlspecialchars($response);
-echo '</pre>';
+echo "<h2>HTTP Headers</h2><pre>";
+print_r($http_response_header ?? []);
+echo "</pre>";
 
-echo '<h2>Decoded MapID JSON</h2>';
-echo '<pre>';
-print_r(json_decode($response, true));
-echo '</pre>';
+echo "<h2>Raw Response</h2><pre>";
+
+if ($response === false) {
+    echo "Request failed.";
+} else {
+    echo htmlspecialchars($response);
+}
+
+echo "</pre>";
