ba0adf9b7e96196b07c127109fd1bc9a97904d0c
Steve Skye <notyou61@gmail.com>
2026-07-08 06:01:11 -0700

    Chore: no effective changes detected

---
+++ api/testAPN.php
@@ -0,0 +1,15 @@
+$apn = '17342369A';
+
+$url = 'https://mcassessor.maricopa.gov/mapid/parcel/' . urlencode($apn);
+
+$response = file_get_contents($url);
+
+echo '<h2>MapID Response</h2>';
+echo '<pre>';
+echo htmlspecialchars($response);
+echo '</pre>';
+
+echo '<h2>Decoded MapID JSON</h2>';
+echo '<pre>';
+print_r(json_decode($response, true));
+echo '</pre>';
