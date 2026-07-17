42cf7a4df2c2eab9159be3e2dbe40c7f94c631de
Steve Skye <steve.skye@skyelighting.com>
2026-07-17 10:44:06 -0700

    Chore: no effective changes detected

---
+++ testQueenCreek.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+// Enable error reporting for testing
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+header('Content-Type: application/json; charset=utf-8');
+
+require_once __DIR__ . '/resolveZoning.php';
+
+// Queen Creek test coordinates from zoning.json
+$latitude = 33.248311;
+$longitude = -111.634122;
+$jurisdiction = 'Queen Creek';
+
+$result = resolveZoning($jurisdiction, $latitude, $longitude);
+
+echo json_encode($result, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
