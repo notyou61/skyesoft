0a35c5fa5bbe5e779bd0e938c614ded929862eb4
Steve Skye <steve.skye@skyelighting.com>
2026-07-17 10:48:09 -0700

    Update: update testQueenCreek.php

--- testQueenCreek.php
+++ testQueenCreek.php
@@ -8,7 +8,7 @@ error_reporting(E_ALL);
 
 header('Content-Type: application/json; charset=utf-8');
 
-require_once __DIR__ . '/resolveZoning.php';
+require_once __DIR__ . '/api/utils/resolveZoning.php';
 
 // Queen Creek test coordinates from zoning.json
 $latitude = 33.248311;
