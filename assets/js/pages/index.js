a264acfb1a57bcf347d09dd690e19140922089d0
Steve Skye <notyou61@gmail.com>
2026-08-02 09:29:56 -0700

    Update: update assets/js/pages/index.js

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -8580,23 +8580,32 @@ window.SkyIndex = {
 // #region 🧾 Page Registration
 window.SkyeApp.registerPage('index', window.SkyIndex);
 
-if (typeof SkyIndex.renderEntityPage === 'function') {
-    SkyWorkspace.registerPage('entity', SkyIndex.renderEntityPage.bind(SkyIndex));
-}
-if (typeof SkyIndex.renderLocationsPage === 'function') {
-    SkyWorkspace.registerPage('locations', SkyIndex.renderLocationsPage.bind(SkyIndex));
-}
-if (typeof SkyIndex.renderLocationPage === 'function') {
-    SkyWorkspace.registerPage('location', SkyIndex.renderLocationPage.bind(SkyIndex));
-}
-if (typeof SkyIndex.renderContactsPage === 'function') {
-    SkyWorkspace.registerPage('contacts', SkyIndex.renderContactsPage.bind(SkyIndex));
-}
-if (typeof SkyIndex.renderContactPage === 'function') {
-    SkyWorkspace.registerPage('contact', SkyIndex.renderContactPage.bind(SkyIndex));
-}
+if (window.SkyWorkspace) {
+    if (typeof SkyIndex.renderEntityPage === 'function') {
+        SkyWorkspace.registerPage('entity', SkyIndex.renderEntityPage.bind(SkyIndex));
+    }
+    if (typeof SkyIndex.renderLocationsPage === 'function') {
+        SkyWorkspace.registerPage('locations', SkyIndex.renderLocationsPage.bind(SkyIndex));
+    }
+    if (typeof SkyIndex.renderLocationPage === 'function') {
+        SkyWorkspace.registerPage('location', SkyIndex.renderLocationPage.bind(SkyIndex));
+    }
+    if (typeof SkyIndex.renderContactsPage === 'function') {
+        SkyWorkspace.registerPage('contacts', SkyIndex.renderContactsPage.bind(SkyIndex));
+    }
+    if (typeof SkyIndex.renderContactPage === 'function') {
+        SkyWorkspace.registerPage('contact', SkyIndex.renderContactPage.bind(SkyIndex));
+    }
 
-Object.keys(SkyWorkspace._registry)
+    console.log('[SkyWorkspace] registered:', Object.keys(SkyWorkspace._registry));
+    console.log('[SkyWorkspace] renderers:', {
+        entity:    typeof SkyIndex.renderEntityPage,
+        locations: typeof SkyIndex.renderLocationsPage,
+        location:  typeof SkyIndex.renderLocationPage,
+        contacts:  typeof SkyIndex.renderContactsPage,
+        contact:   typeof SkyIndex.renderContactPage
+    });
+}
 // #endregion
 
 // #region 🗺️ Google Maps Dual View Initializer (Map + Street View)
